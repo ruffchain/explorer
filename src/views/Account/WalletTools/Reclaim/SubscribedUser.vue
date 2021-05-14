13bbbcb60480113c630308bb3f9ec7b48d6c9b40
yango3 <o3.yang@hotmail.com>
2021-05-14 11:24:12 +0800

    Run deposit OK

--- src/views/Account/WalletTools/Reclaim/SubscribedUser.vue
+++ src/views/Account/WalletTools/Reclaim/SubscribedUser.vue
@@ -39,8 +39,12 @@
         label-width="150px"
       >
         <el-form-item label="MetaMask">
-          <el-button :type="metaMaskConnected" @click="enableMetaMask()" >
-            Connect MetaMask
+          <el-button
+            :disabled="bButtonDisabled"
+            :type="metaMaskConnected"
+            @click="enableMetaMask()"
+          >
+            {{ textMetaMask }}
           </el-button>
         </el-form-item>
         <el-form-item prop="amount" :rules="amountRules" :label="strAmount">
@@ -59,6 +63,17 @@
       </el-form>
     </section>
     <TransactionResult v-if="result" :data="result" />
+
+    <ConfirmTx
+      :visible.sync="showConfirmTx"
+      :tx="txData"
+      @confirm="confirmSendTx"
+    />
+    <AppDialog
+      title="RuffToken depositTo()"
+      @cleanForm="cleanTable(), (apploading = false)"
+      v-if="apploading"
+    />
   </div>
 </template>
 
@@ -67,8 +82,13 @@ import TransactionResult from '../TransactionResult'
 import LoadingContainer from '@/components/LoadingContainer'
 import * as rules from '../form-rules.js'
 import { ethers } from 'ethers'
-import {abi} from './RuffTokenAbi'
+import { abi } from './RuffTokenAbi'
+import ConfirmTx from '../ConfirmTx'
+import AppDialog from '../../../../components/AppDialog'
+
 const BigNumber = require('bignumber.js')
+
+const RuffTokenContract = '0x3fb9c619bc31d5e98aeb698cea697f6010f9f8f4'
 
 export default {
   props: {
@@ -77,25 +97,30 @@ export default {
   components: {
     LoadingContainer,
     TransactionResult,
+    ConfirmTx,
+    AppDialog
   },
   data() {
     return {
       loading: false,
       account: '',
       metaMaskConnected: 'warning',
-      textConnected: '',
+      // textConnected: '',
       formData: {
         amount: ''
       },
       txLoading: false,
-      result:'',
-      contract:null,
+      result: '',
+      contract: null,
+      bButtonDisabled: true,
+      textMetaMask: '',
+      txData: {},
+      showConfirmTx: false,
+      apploading: false
     }
   },
   computed: {
     amountRules() {
-      // let maxCount = rules.amountOver(this.value)
-
       return [...this.formRules.amount]
     },
     strTxConfirm() {
@@ -112,13 +137,13 @@ export default {
     this.formRules = {
       amount: [required, gt0, rules.amountTooBig()]
     }
+    this.checkMetaMask()
   },
   methods: {
-    async enableMetaMask() {
-      console.log('Enable MetaMask')
-      if (this.metaMaskConnected === 'success') {
-        return
-      }
+    async checkMetaMask() {
+      console.log('Check MetaMask')
+      this.textMetaMask = 'Connect MetaMask'
+
       if (typeof window.ethereum !== 'undefined') {
         const accounts = await ethereum.request({
           method: 'eth_requestAccounts'
@@ -128,12 +153,15 @@ export default {
         if (this.account) {
           this.metaMaskConnected = 'success'
           console.log('account: ', this.account)
+          this.bButtonDisabled = true
+          this.textMetaMask = 'MetaMast Connected'
+        } else {
+          this.bButtonDisabled = false
         }
         ethereum.on('accountsChanged', accountsNew => {
           this.account = accountsNew[0]
           console.log('account: ', this.account)
         })
-        
       } else {
         console.log('Please install MetaMask')
         this.metaMaskConnected = 'danger'
@@ -142,28 +170,66 @@ export default {
         }
       }
     },
+    async enableMetaMask() {
+      console.log('Enable MetaMask')
+      this.result = { message: '' }
+      if (typeof window.ethereum !== 'undefined') {
+        const accounts = await ethereum.request({
+          method: 'eth_requestAccounts'
+        })
+        console.log(typeof accounts[0])
+        this.account = accounts[0]
+        if (this.account) {
+          this.metaMaskConnected = 'success'
+          console.log('account: ', this.account)
+          this.textMetaMask = 'MetaMast Connected'
+        }
+      }
+    },
     async confirm() {
       console.log('confirm()')
-      const provider = new  ethers.providers.Web3Provider(window.ethereum)
-      // this.contract = new ethers.Contract(
-      //     "0x3fb9c619bc31d5e98aeb698cea697f6010f9f8f4",
-      //     abi,
-      //     provider
-      //   )
-      // let result = await this.contract.decimals()
-      // console.log("decimals:", result.toString())
-      let signer = provider.getSigner();
+      let val = parseFloat(this.formData.amount)
+      console.log(val)
+      if (val <= 0) {
+        this.result = {
+          message: 'Invalid amount' + this.formData.amount
+        }
+        return
+      }
 
-      this.contract = new ethers.Contract(
-        "0x3fb9c619bc31d5e98aeb698cea697f6010f9f8f4",
-        abi,
-        signer
-      )
-      const value = new BigNumber(10).pow(18).times(10.123)
-      let tx = await this.contract.transfer("0x9801c7Af4bc83073d41704cd6A33A81975cecF84",value.toFixed());
-      let receipt = await tx.wait()
-      console.log("transfer:", receipt)
       // console.log("num:", ethers.utils.parseEther("0.2").toBigInt())
+      this.txData = {
+        method: 'depositTo',
+        ruffAddr: this.ruffAddr,
+        value: val
+      }
+      this.showConfirmTx = true
+    },
+    async confirmSendTx() {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+        let signer = provider.getSigner()
+
+        this.contract = new ethers.Contract(RuffTokenContract, abi, signer)
+        const value = new BigNumber(10).pow(18).times(new BigNumber(this.formData.amount))
+        let tx = await this.contract.depositTo(value.toFixed(),this.ruffAddr)
+        this.apploading = true
+        let receipt = await tx.wait()
+        console.log('depositTo receipt:', receipt)
+
+        this.result = {
+          message: receipt.status === 1 ? 'Send OK' : 'Send Fail'
+        }
+      } catch (e) {
+        console.log(e)
+        this.result = {
+          message: 'Error Encountered'
+        }
+      } finally {
+        this.apploading = false
+        this.formData.amount = ''
+      }
     }
   }
 }
