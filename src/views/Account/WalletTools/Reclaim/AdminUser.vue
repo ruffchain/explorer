28e0df26987eda3898c817f66c67565c46521e22
yango3 <o3.yang@hotmail.com>
2021-05-17 17:22:00 +0800

    update reclaim admin radio buttons

--- src/views/Account/WalletTools/Reclaim/AdminUser.vue
+++ src/views/Account/WalletTools/Reclaim/AdminUser.vue
@@ -38,6 +38,14 @@
       <section>
         <el-alert :closable="false" :title="strAlert" type="info" show-icon>
       </el-alert>
+      <div class="top-radio-selecter">
+        <el-radio-group size="medium" v-model="action" @change="actionChange">
+          <el-radio-button :label="actionInvalid"></el-radio-button>
+          <el-radio-button :label="actionAccepted"></el-radio-button>
+          <el-radio-button :label="actionCompleted"></el-radio-button>
+          <el-radio-button :label="actionRejected"></el-radio-button>
+        </el-radio-group>
+      </div>
 
       </section>
 
@@ -63,6 +71,7 @@ export default {
   data(){
     return{
       strAlert: '',
+      action: '',
       loading: false,
       page:1,
       pageSize:5,
@@ -75,6 +84,58 @@ export default {
   computed:{
     dataReclaims() {
       return getDataReclaims(this.reclaims.data)
+    },
+    actionInvalid() {
+      return 'invalid'
+    },
+    actionAccepted() {
+      return 'accepted'
+    },
+    actionCompleted() {
+      return 'completed'
+    },
+    actionRejected() {
+      return 'rejected'
+    },
+    strActionInvalid() {
+      return '管理员页面: ' + '用户提交的未验证的请求'
+    },
+    strActionAccepted() {
+      return '管理员页面: ' + '已批准的请求'
+    },
+    strActionCompleted() {
+      return '管理员页面: ' + '已完成的请求'
+    },
+    strActionRejected() {
+      return '管理员页面: ' + '被驳回的请求'
+    },
+  },
+  beforeMount(){
+    this.strAlert = this.strActionInvalid
+    this.action = this.actionInvalid
+  },
+  mounted(){},
+  methods:{
+     actionChange() {
+      console.log('change')
+
+      if (this.action === this.actionInvalid) {
+        this.strAlert = this.strActionInvalid
+        this.updateInvalidReclaim()
+        this.page = 1
+      }  else if (this.action === this.actionAccepted) {
+        this.strAlert = this.strActionAccepted
+        this.updateAcceptedReclaim()
+        this.page = 1
+      } else if (this.action === this.actionCompleted) {
+        this.strAlert = this.strActionCompleted
+        this.updateCompletedReclaim()
+        this.page = 1
+      } else if (this.action === this.actionRejected) {
+        this.strAlert = this.strActionRejected
+        this.updateRejectedReclaim()
+        this.page = 1
+      }
     },
   }
 }
